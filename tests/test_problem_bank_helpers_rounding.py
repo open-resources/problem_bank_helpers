e4d5172867016f867e85bb66ba7eb6dd8629d9f1
amurph24 <amurph@student.ubc.ca>
2023-06-17 23:13:15 -0700

    add roundp and round_str tests

--- tests/test_problem_bank_helpers_rounding.py
+++ tests/test_problem_bank_helpers_rounding.py
@@ -96,7 +96,7 @@ def variables():
     inputs = test_values
     return inputs
 
-
+# Test sigfigs function
 @pytest.mark.parametrize(
     "test_group",
     group_names
@@ -124,57 +124,72 @@ def test_sigfigs(test_group, variables):
             correct_sigfigs = i+1
         else:
             pytest.fail(f"test group is not defined (got: '{test_group}')")
-        assert (pbh.sigfigs(test_input) == correct_sigfigs), f"sigfig({test_input}) = {pbh.sigfigs(test_input)} did not match expected output: {correct_sigfigs}"
+        assert (pbh.sigfigs(test_input) == correct_sigfigs)
 
 
+# Test round_sig function
 @pytest.mark.parametrize(
     "test_input, expected_output",
     [(123456789, [100000000, 120000000, 123000000, 123500000, 123460000, 123457000, 123456800, 123456790, 123456789]),
-     (0.123456789, [0.1, 0.12, 0.123, 0.1235, 0.12346, 0.123457, 0.1234568, 0.12345679, 0.123456789])]
+     (0.123456789, [0.1, 0.12, 0.123, 0.1235, 0.12346, 0.123457, 0.1234568, 0.12345679, 0.123456789]),
+     (0.1, [0.1, 0.10, 0.100, 0.1000, 0.10000, 0.100000, 0.1000000, 0.10000000, 0.100000000])]
 )
 def test_roundsig(test_input, expected_output):
     for i in range(9):
-        assert (pbh.round_sig(test_input, i + 1) == expected_output[i]), f"round_sig({test_input}, {i+1}) = {pbh.round_sig(test_input, i+1)} did not match expected output: {expected_output[i]}"
+        assert (pbh.round_sig(test_input, i + 1) == expected_output[i])
 
-
+# Test num_as_str function
 @pytest.mark.parametrize(
     "test_input, expected_output",
-    [(123.123456789, [123, 123.1, 123.12, 123.123, 123.1235, 123.12346, 123.123457, 123.1234568, 123.12345679, 123.123456789]),
+    [(123.456789000, ["123", "123.5", "123.46", "123.457", "123.4568", "123.45679", "123.456789", "123.4567890", "123.45678900", "123.456789000"]),
      (0.123456789, [0, 0.1, 0.12, 0.123, 0.1235, 0.12346, 0.123457, 0.1234568, 0.12345679, 0.123456789]),
-     (0.000123456, [0, 0, 0, 0, 0.0001, 0.00012, 0.000123, 0.0001234, 0.00012345, 0.000123456])]
+     (0.000123456, [0, 0, 0, 0, 0.0001, 0.00012, 0.000123, 0.0001234, 0.00012345, 0.000123456]),
+     (4.967489, ["5", "5.0", "4.97", "4.967", "4.9675", "4.96749", "4.967489", "4.9674890", "4.96748900", "4.967489000"])]
 )
 def test_numasstr(test_input, expected_output):
     if abs(test_input >= 1):
         for i in range(10):
-            assert (pbh.num_as_str(test_input, i) == str(expected_output[i])), f"num_as_str({test_input}, {i}) = {pbh.num_as_str(test_input, i)} did not match expected output: {expected_output[i]}"
+            assert (pbh.num_as_str(test_input, i) == str(expected_output[i]))
 
-@pytest.mark.parametrize(
-    "test_input, expected_output",
-    []
-)
-def test_round_sig(test_input, expected_output):
-    assert pbh.round_sig(test_input) == expected_output, "input: {" + test_input + "} did not match expected output: {" + expected_output + "}"
+# Test roundp function
+def test_roundp():
+    # Test rounding a float with specified sigfigs
+    assert pbh.roundp(123.456, sigfigs=2) == 120.0
 
+    # Test rounding an integer with specified decimals
+    assert pbh.roundp(987, decimals=2) == 987.0
 
-@pytest.mark.parametrize(
-    "test_input, expected_output",
-    []
-)
-def test_roundp(test_input, expected_output):
-    assert pbh.roundp(test_input) == expected_output, "input: {" + test_input + "} did not match expected output: {" + expected_output + "}"
+    # Test rounding a string with default sigfigs
+    assert pbh.roundp('99.8765') == '100'
 
+    # Test rounding a float with format='std'
+    assert pbh.roundp(0.123, format='std') == 0.12
 
-@pytest.mark.parametrize(
-    "test_input, expected_output",
-    []
-)
-def test_round_str(test_input, expected_output):
-    assert pbh.round_str(test_input) == expected_output, "input: {" + test_input + "} did not match expected output: {" + expected_output + "}"
+    # Test rounding a float with format='English'
+    assert pbh.roundp(12345.6789, format='English') == 12300.0
 
+    # Test rounding a float with format='sci'
+    assert pbh.roundp(0.000123, format='sci') == '1.20e-04'
 
-@pytest.mark.parametrize(
-    "test_input, expected_output",
-    []
-)
-def test_num_as_str(test_input, expected_output):
-    assert pbh.num_as_str(test_input) == expected_output, "input: {" + test_input + "} did not match expected output: {" + expected_output + "}"
+# Test round_str function
+def test_roundstr():
+    # Test passing a string as the argument
+    assert pbh.round_str('Hello') == 'Hello'
+
+    # Test rounding a float without specifying sigfigs or format
+    assert pbh.round_str(123.456) == '123'
+
+    # Test rounding a float with specified sigfigs
+    assert pbh.round_str(123.456, sigfigs=2) == 120.0
+
+    # Test rounding a float with specified format='English'
+    assert pbh.round_str(12345.6789, format='English') == 12300.0
+
+    # Test rounding a float with specified format='sci'
+    assert pbh.round_str(0.000123, format='sci') == '1.20e-04'
+
+    # Test rounding a float with specified sigfigs and format='std'
+    assert pbh.round_str(0.123, sigfigs=3, format='std') == 0.120
+
+    # Test rounding an integer with specified decimals and format='sci'
+    assert pbh.round_str(987, decimals=2, format='sci') == '9.87e+02'
